9cbbefcb5b45d54dd5d60817a7078d2079cef2c2
Tolvic <terence.h.mace@gmail.com>
2018-11-26 16:58:05 +0000

    db integration - can read from db

--- Acebook/Acebook/Controllers/PostController.cs
+++ Acebook/Acebook/Controllers/PostController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Acebook.Models;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +12,32 @@ namespace Acebook.Controllers
 {
     public class PostController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+
+        private readonly AcebookContext _context;
+
+        public PostController(AcebookContext context)
         {
-            //View all posts
-            return View();
+            _context = context;
+
+            //if (_context.AcebookItems.Count() == 0)
+            //{
+            //    _context.AcebookItems.Add(new Post { message = "Item1" });
+            //    _context.SaveChangesS ();
+            //}
+        }
+
+            // GET: /<controller>/
+        //    public IActionResult Index()
+        //{
+        //    return _context.AcebookItems.ToList;
+        //    //View all posts
+        //    //return View();
+        //}
+
+        [HttpGet]
+        public ActionResult<List<Post>> GetAll()
+        {
+            return _context.posts.ToList();
         }
 
         // GET: /<controller>/:id
--- Acebook/Acebook/Startup.cs
+++ Acebook/Acebook/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Acebook.Models;
 
 namespace Acebook
 {
@@ -24,15 +26,11 @@ namespace Acebook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                // Add framework services.
+                services.AddDbContext<AcebookContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
